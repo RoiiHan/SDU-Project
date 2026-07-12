2a58bb773df1458001ef8ed9096eb8d5877691a4
RoihanTauhid <roihanntauhidd@gmail.com>
2026-07-13 02:42:21 +0700

    server perbaikan

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,37 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "sdu_db",
+console.log("DEBUG DB_HOST:", process.env.DB_HOST);
+console.log("DEBUG DB_PORT:", process.env.DB_PORT);
+console.log("DEBUG DB_USER:", process.env.DB_USER);
+console.log("DEBUG DB_NAME:", process.env.DB_NAME);
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
+  },
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.log("koneksi gagal");
     console.log(err);
     return;
   }
   console.log("Database berhasil terhubung");
+  connection.release();
 });
 
 app.use(cors());
